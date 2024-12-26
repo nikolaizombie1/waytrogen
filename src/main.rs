b23096697693c33c96538254def6cb5bfcf650fa
nikolaizombie1 <fabio.matos999@gmail.com>
2024-12-26 14:31:27 -0500

    Created image grid.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use glib::clone;
-use gtk::{prelude::*, subclass::text_view};
-use std::cell::Cell;
-use std::rc::Rc;
+use std::sync::OnceLock;
 
-use gtk::{glib, Application, ApplicationWindow};
+use gtk::{
+    gdk::Texture, gio::{self, ListStore}, glib::{self, subclass::Signal, BoxedAnyObject, Value, closure_local, clone}, prelude::*, Align, Application, ApplicationWindow, Box, Button, GridView, Image, ListItem, Orientation, ScrolledWindow, SignalListItemFactory, SingleSelection
+};
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld1";
 
@@ -18,48 +17,65 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    let ui_src = include_str!("waypaper.ui");
-    let builder = gtk::Builder::from_string(&ui_src);
-    let window = builder
-        .object::<gtk::ApplicationWindow>("window")
-        .expect("Couldn't get window");
-    window.set_application(Some(app));
-    let text_view = builder
-        .object::<gtk::TextView>("text_view")
-        .expect("Couldn't get text_view");
-    let text_view_copy = text_view.clone();
+    let image_list_store = ListStore::new::<Image>();
+    for _ in 0..100 {
+        let image = Image::from_paintable(Some(
+            &Texture::from_file(&gio::File::for_path("test.png")).unwrap(),
+        ));
+        // button.set_child(Some(&image));
+        image_list_store.append(&image);
+    }
 
-    let dialog = gtk::FileDialog::new();
+    let selection = SingleSelection::new(Some(image_list_store));
+    let image_signal_list_item_factory = SignalListItemFactory::new();
+    image_signal_list_item_factory.connect_setup( move |_factory, item| {
+        let item = item.downcast_ref::<ListItem>().unwrap();
+        item.set_child(Some(&Button::builder().vexpand(true).hexpand(true).can_shrink(true).build()));
+    });
 
-    dialog.select_folder(
-        Some(&window),
-        gtk::gio::Cancellable::NONE,
-        move |o| match o {
-            Ok(f) => {
-                text_view.buffer().set_text(
-                    &String::from_utf8(
-                        f.path()
-                            .unwrap()
-                            .canonicalize()
-                            .unwrap()
-                            .as_os_str()
-                            .as_encoded_bytes()
-                            .to_vec(),
-                    )
-                    .unwrap(),
-                );
-            }
-            Err(_) => todo!(),
-        },
-    );
+    image_signal_list_item_factory.connect_bind(move |_factory, item| {
+        let item = item.downcast_ref::<ListItem>().unwrap();
+        let child = item.child();
+        let child = child.and_downcast::<Button>().unwrap();
+        let entry = item.item().and_downcast::<Image>().unwrap();
+        child.set_size_request(200, 200);
+        child.set_child(Some(&entry));
+    });
 
-    let y = text_view_copy
-        .buffer()
-        .text(
-            &text_view_copy.buffer().start_iter(),
-            &text_view_copy.buffer().end_iter(),
-            false,
-        );
-    println!("{}", y.as_str());
+
+    let image_grid = GridView::builder().model(&selection).factory(&image_signal_list_item_factory).hexpand(true).vexpand(true).build();
+    let scrolled_winow = ScrolledWindow::builder().child(&image_grid).valign(Align::Start).halign(Align::Center).propagate_natural_height(true).propagate_natural_width(true).build();
+    scrolled_winow.set_size_request(900, 900);
+    let open_folder_button = Button::builder()
+        .margin_top(12)
+        .margin_start(12)
+        .margin_bottom(12)
+        .margin_end(12)
+        .halign(Align::End)
+        .build();
+    let application_box = Box::builder()
+        .margin_top(12)
+        .margin_start(12)
+        .margin_bottom(12)
+        .margin_end(12)
+        .orientation(Orientation::Vertical)
+        .build();
+    application_box.append(&scrolled_winow);
+    application_box.append(&open_folder_button);
+
+    application_box.set_width_request(1000);
+
+    open_folder_button.set_label("Image Folder");
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Waytrogen")
+        .child(&application_box)
+        .build();
+
+        let window_copy = window.clone();
+    //application_box.connect_closure("size-allocate", true,  c
+    
+
     window.present();
 }
