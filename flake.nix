bb7da7533c9669560c71283abafca160ade34391
nikolaizombie1 <fabio.matos999@gmail.com>
2026-02-21 11:08:08 -0500

    Bump version 0.9.0.

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
         packages = rec {
           waytrogen = pkgs.rustPlatform.buildRustPackage {
             pname = "waytrogen";
-            version = "0.8.0";
+            version = "0.9.0";
             src = ./.;
 
             cargoLock = {
